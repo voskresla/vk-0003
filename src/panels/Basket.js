2c8166a37424d6d7e4a2c83d1840df4cea056485
MoksS <MoksShuv@yandex.ru>
2020-09-09 22:05:48 +0300

    сменил тип у инпута

--- src/panels/Basket.js
+++ src/panels/Basket.js
@@ -127,6 +127,7 @@ const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
           <span>Назначить</span>
           <input
             value={time}
+            type="time"
             onFocus={() => {
               setFaster(false);
             }}
